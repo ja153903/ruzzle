419110433dc365a6125805f163905399f09f8e98
ja153903 <jaimeabbariao@gmail.com>
2025-12-03 01:25:35 -0500

    updating with day 3 solution

--- src/aoc/mod.rs
+++ src/aoc/mod.rs
@@ -1,5 +1,7 @@
+pub mod template;
 pub mod yr2015_d01;
 pub mod yr2015_d02;
 pub mod yr2024_d22;
 pub mod yr2025_d01;
 pub mod yr2025_d02;
+pub mod yr2025_d03;
---
+++ src/aoc/yr2025_d03.rs
@@ -0,0 +1,127 @@
+#![allow(dead_code, unused_imports)]
+
+use anyhow::anyhow;
+use itertools::Itertools;
+use std::collections::VecDeque;
+
+struct Solver;
+
+impl Solver {
+    fn read() -> &'static str {
+        include_str!("data/yr2025_d03.in")
+    }
+
+    fn readlines<'a>() -> Vec<&'a str> {
+        Self::read().lines().collect()
+    }
+
+    fn part1() -> anyhow::Result<i64> {
+        Ok(Self::readlines()
+            .iter()
+            .map(|pack| {
+                // for each pack, we want to keep track of a queue of size 2
+                let mut largest_items: VecDeque<char> = VecDeque::new();
+                pack.chars().for_each(|ch| {
+                    // if the second value in the queue is larger than the
+                    // first value in the queue, then we'll bump it whenever we see
+                    // a larger item
+
+                    if largest_items.len() <= 1 {
+                        largest_items.push_back(ch);
+                    } else {
+                        // remove the first two elements to compare
+                        let fst = largest_items.pop_front();
+                        let snd = largest_items.pop_front();
+
+                        if let (Some(fst), Some(snd)) = (fst, snd) {
+                            // if fst is less than snd, then we can just accept
+                            // anything that comes
+                            if fst < snd {
+                                largest_items.push_front(snd);
+                                largest_items.push_back(ch);
+                            // if fst is greater than snd but snd is less than ch
+                            // then we just add ch
+                            } else if snd < ch {
+                                largest_items.push_front(fst);
+                                largest_items.push_back(ch);
+                            // if fst is greater than snd and snd is greater than ch
+                            } else {
+                                largest_items.push_front(fst);
+                                largest_items.push_back(snd);
+                            }
+                        }
+                    }
+                });
+
+                let fst = largest_items.pop_front();
+                let snd = largest_items.pop_front();
+
+                if let (Some(fst), Some(snd)) = (fst, snd) {
+                    let combined: String = vec![fst, snd].into_iter().collect();
+                    if let Ok(parsed) = combined.parse::<i64>() {
+                        return parsed;
+                    }
+                }
+
+                0
+            })
+            .sum())
+    }
+
+    fn part2() -> anyhow::Result<i64> {
+        Ok(Self::readlines()
+            .iter()
+            .map(|pack| {
+                let mut largest_items: VecDeque<char> = VecDeque::new();
+                let mut deletes_remaining = pack.len() - 12;
+                pack.chars().for_each(|ch| {
+                    while let Some(&back) = largest_items.back()
+                        && back < ch
+                        && deletes_remaining > 0
+                    {
+                        largest_items.pop_back();
+                        deletes_remaining -= 1;
+                    }
+
+                    largest_items.push_back(ch);
+                });
+
+                while largest_items.len() != 12 {
+                    largest_items.pop_back();
+                }
+
+                let mut chars: Vec<char> = Vec::new();
+                while !largest_items.is_empty() {
+                    let front = largest_items.pop_front();
+                    if let Some(front) = front {
+                        chars.push(front);
+                    }
+                }
+
+                let combined: String = chars.into_iter().collect();
+                if let Ok(parsed) = combined.parse::<i64>() {
+                    return parsed;
+                }
+
+                0
+            })
+            .sum())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solver;
+
+    #[test]
+    pub fn test_part1() {
+        let result = Solver::part1().expect("Part 1 should complete successfully");
+        assert_eq!(result, 17766);
+    }
+
+    #[test]
+    pub fn test_part2() {
+        let result = Solver::part2().expect("Part 2 should complete successfully");
+        assert_eq!(result, 176582889354075);
+    }
+}
